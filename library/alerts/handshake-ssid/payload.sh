3ffb1221fa7a6438af8820bd8c097995a41f1f14
RootJunky <rootjunky@gmail.com>
2025-12-26 17:03:09 -0600

    Update author in payload.sh script

--- library/alerts/handshake-ssid/payload.sh
+++ library/alerts/handshake-ssid/payload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Title: Example Handshake Capture Alert
 # Description: Alert human readable summary
-# Author: Hak5Darren Modified
+# Author: RootJunky
 # Version: 1
 
 PCAP="$_ALERT_HANDSHAKE_PCAP_PATH"
