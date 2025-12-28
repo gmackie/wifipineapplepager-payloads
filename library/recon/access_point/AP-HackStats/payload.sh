4236c890a215c266f26979d853de1d1e8b1802b2
Unit98.1 <104841370+Unit981@users.noreply.github.com>
2025-12-28 20:35:21 +0800

    Updated payload.sh
    Made simple, removed bloat & used ALERT instead of just logging everything.

--- library/recon/access_point/AP-HackStats/payload.sh
+++ library/recon/access_point/AP-HackStats/payload.sh
@@ -2,32 +2,18 @@
 # Title: Targeted AP HackStats
 # Author: Unit981
 # Description: Get handshake and pcap stats for selected AP
-# Version: 1.0
+# Version: 1.1
 
-/usr/bin/LOG "Dumping Selected AP information"
-LOG "AP OUI: $_RECON_SELECTED_AP_OUI"
-LOG "AP BSSID: $_RECON_SELECTED_AP_BSSID"
-LOG "AP SSID: $_RECON_SELECTED_AP_SSID"
-LOG "AP Hidden: $_RECON_SELECTED_AP_HIDDEN"
-LOG "AP Channel: $_RECON_SELECTED_AP_CHANNEL"
-LOG "AP Encryption Type: $_RECON_SELECTED_AP_ENCRYPTION_TYPE"
-LOG "AP Client Count: $_RECON_SELECTED_AP_CLIENT_COUNT"
-LOG "AP RSSI: $_RECON_SELECTED_AP_RSSI"
-LOG "AP Timestamp: $_RECON_SELECTED_AP_TIMESTAMP"
-LOG "AP Frequency: $_RECON_SELECTED_AP_FREQ"
-LOG "AP Packets: $_RECON_SELECTED_AP_PACKETS"
-
-
-#Base directory
+#Setting directory
 HANDSHAKE_DIR="/root/loot/handshakes/"
+
+#Making sure BSSID/MAC is searchable
 bssid_clean=$(printf "%s" "$_RECON_SELECTED_AP_BSSID" | sed 's/[[:space:]]//g')
 bssid_upper=${bssid_clean^^}
 
-#Count files containing MAC anywhere in filename
+#Count files containing MAC anywhere in the filename
 handshake_count=$(find "$HANDSHAKE_DIR" -type f -name "*${bssid_upper}*.22000" 2>/dev/null | wc -l)
 pcap_count=$(find "$HANDSHAKE_DIR" -type f -name "*${bssid_upper}*.pcap" 2>/dev/null | wc -l)
 
-
 #Final output
-LOG "Handshake Count: $handshake_count"
-LOG "PCAP Count: $pcap_count"
+ALERT "#@ HACK THE PLANET @# \n\n AP SSID: $_RECON_SELECTED_AP_SSID \n "AP BSSID: $_RECON_SELECTED_AP_BSSID" \n Handshake Count: $handshake_count \n PCAP Count: $pcap_count"
